e3485b832380c76ef1aa6ae67307865614ddab36
Jacob <jacob@MacBook-Pro-680.local>
2025-09-11 14:42:30 -0400

    9/11

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+
+  digitalWrite(4, HIGH); //Set enable high
+  digitalWrite(6, LOW);
+  analogWrite(5, 250);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
