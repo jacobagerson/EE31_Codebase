4a59d605dda1bd566c3af3bdfe4dfea75b979821
Cooper Bailey <cooperbailey@Coopers-MacBook-Pro.local>
2025-09-16 13:37:40 -0400

    resolve

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <Arduino.h>
 
-
 void setup() {
   // put your setup code here, to run once:
   pinMode(4, OUTPUT);
@@ -15,4 +14,3 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 }
-
